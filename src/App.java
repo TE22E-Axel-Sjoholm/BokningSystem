961131169ae517c1f163ce4f8729e78f21065fd3
Axel Sjöholm <Axel.sjoholm@elev.ga.ntig.se>
2024-03-29 00:37:41 +0100

    lagt till info, gjort unserInput checkern i sen egna void

--- src/App.java
+++ src/App.java
@@ -2,18 +2,48 @@ import java.util.Scanner;
 
 public class App {
     public static int scene = 0;
+    public static boolean bokstav;
+    public static String[][] data;
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
-        boolean bokstav;
+        
         while(scene == 0) {
             bokstav = false;
             System.out.println(" ");
             System.out.println("1. Boka resa");
             System.out.println("2. Se lediga platser");
             System.out.println("3. Info");
-            System.out.println("4. Avsluta");
+            System.out.println("4. Sök efter resa");
+            System.out.println("5. Avsluta");
+            String userInput = scanner.nextLine();
+            checkIfLetter(userInput);
+            if(bokstav == false && Integer.parseInt(userInput) == 5){
+                System.exit(0);
+            } else if(bokstav == false && Integer.parseInt(userInput) >= 1 && Integer.parseInt(userInput) <= 3){
+                scene = Integer.parseInt(userInput);
+            }
+        }
+    }
+
+    public static void infoMeny(){
+        while (scene == 3) {
+            bokstav = false;
+            System.out.println(" ");
+            System.out.println("");
+            System.out.println("");
+            System.out.println("");
+            System.out.println("Tryck enter för att avlsuta");
             System.out.println(" ");
             String userInput = scanner.nextLine();
+            checkIfLetter(userInput);
+
+        }
+    }
+    
+    static void checkIfLetter(String userInput){
+        if(userInput.isEmpty()){
+            System.out.println("Skriv en siffra, inte en bokstav");
+        } else{
             for (int i = 0; i < userInput.length(); i++) {
                 char letter = userInput.charAt(i);
                 if(!Character.isDigit(letter)){
@@ -22,13 +52,6 @@ public class App {
                     i = (userInput.length()) + 1;
                 } 
             }
-            if(bokstav == false && Integer.parseInt(userInput) == 4){
-                System.exit(0);
-            } else if(bokstav == false && Integer.parseInt(userInput) >= 1 && Integer.parseInt(userInput) <= 3){
-                scene = Integer.parseInt(userInput);
-            }
-            System.out.println(scene);
         }
     }
-
 }
