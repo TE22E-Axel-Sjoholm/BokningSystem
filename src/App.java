443d5fbf2e51142146aaedc3752b325d76b0c24a
Axel Sjöholm <Axel.sjoholm@elev.ga.ntig.se>
2024-03-30 21:44:47 +0100

    fixat error med om man bara trycker enter i huvudmenyn

--- src/App.java
+++ src/App.java
@@ -3,12 +3,14 @@ import java.util.Scanner;
 public class App {
     public static int scene = 0;
     public static boolean bokstav;
-    public static Object[][] data = new Object[0][0];
-    public static String price;
+    public static Object[][] data = new Object[21][5];
+    public static double price;
     static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) throws Exception {
-        for(int i=0; i <= 2; i++){
-            data[i][1] = "0";
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data[i].length; j++) {
+                data[i][j] = 0;
+            }
         }
         while(scene == 0) {
             System.out.println("1. Boka resa");
@@ -20,13 +22,17 @@ public class App {
             String userInput = scanner.nextLine();
             bokstav = false;
             checkIfLetter(userInput);
-            if(bokstav == false && Integer.parseInt(userInput) == 5){
-                System.exit(0);
-            } else if(bokstav == false && Integer.parseInt(userInput) >= 1 && Integer.parseInt(userInput) <= 3){
-                scene = Integer.parseInt(userInput);
+            if(bokstav){
+                continue;
+            } else if(Integer.parseInt(userInput) == 6){
+                    System.exit(0);
+                } else if(Integer.parseInt(userInput) == 1 || userInput == "2" || userInput == "3" || userInput == "4" || userInput == "5"){
+                    scene = 1;
+                }
                 infoMeny();
-                booking();
-            }
+                booking(); 
+            
+            
         }
     }
 
@@ -44,7 +50,7 @@ public class App {
     }
     
     static void checkIfLetter(String userInput){
-        if(userInput.isEmpty()){
+        if(userInput.equals("")){
             System.out.println("Skriv en siffra, inte en bokstav");
         } else{
             for (int i = 0; i < userInput.length(); i++) {
@@ -59,10 +65,10 @@ public class App {
     }
     public static void priceCalc(int userAge){
         if(2024 - (userAge/10000) >= 18){
-            price = "299.90";
+            price = 299.90;
         }
         if(2024 - (userAge/10000) < 18){
-            price = "149.90";
+            price = 149.90;
         }
     }
     public static void booking(){
@@ -72,14 +78,13 @@ public class App {
             bokstav = false;
             checkIfLetter(userPlace);
             if(bokstav){
-                System.out.println("Skriv en siffra, inte en bokstav");
                 continue;
-            } else if(data[Integer.parseInt(userPlace)][1] == "1"){
+            } else if(data[Integer.parseInt(userPlace)][1].equals(1)){
                 System.out.println("Platsen är upptagen, kolla efter lediga platser i huvudmenyn.");
-            } else if(data[Integer.parseInt(userPlace)][1] == "0" || data[Integer.parseInt(userPlace)][1].isEmpty()){
+            } else if(data[Integer.parseInt(userPlace)][1].equals(0)){
                 System.out.println("Platsen är ledig, Skriv ditt namn:");
                 String userName = scanner.nextLine();
-                String userAge = "N/A";
+                String userAge = "";
                 bokstav = false;
                 while(userAge.length() != 8 && bokstav == false) {
                     System.out.println("Vad är ditt födelsedatum? ÅÅÅÅMMDD");
@@ -87,16 +92,17 @@ public class App {
                     bokstav = false;
                     checkIfLetter(userAge);
                     if(userAge.length() == 8 && bokstav == false){
+                        priceCalc(Integer.parseInt(userAge));
                         System.out.println("Platsen är bokad, det kostar "+price+"kr.");
                         data[Integer.parseInt(userPlace)][1] = "1";
                         data[Integer.parseInt(userPlace)][2] = userName;
-                        data[Integer.parseInt(userPlace)][3] = userAge;
+                        data[Integer.parseInt(userPlace)][3] = Integer.parseInt(userAge);
                         data[Integer.parseInt(userPlace)][4] = price;
                     }
                     bokstav = false;
                 }
+                scene = 0;
             }
-
         }
     }
 }
